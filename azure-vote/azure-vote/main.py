1dc00dfa617cb74e6c2dbf0957c1c151852eeb83
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-08-22 09:29:08 +0200

    linting and formatting

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -28,128 +28,155 @@ stats = stats_module.stats
 view_manager = stats.view_manager
 
 # Add Logger for custom Events:
-config_integration.trace_integrations(['logging'])
-config_integration.trace_integrations(['requests']) # <-- this line enables the requests integration
-
+config_integration.trace_integrations(["logging"])
+config_integration.trace_integrations(
+    ["requests"]
+)  # <-- this line enables the requests integration
 
 # Standard Logging
 logger = logging.getLogger(__name__)
-handler = AzureLogHandler(connection_string='InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24')
-handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+handler = AzureLogHandler(
+    connection_string="InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24"
+)
+handler.setFormatter(logging.Formatter("%(traceId)s %(spanId)s %(message)s"))
 logger.addHandler(handler)
-# Logging custom Events 
-logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24'))
+# Logging custom Events
+logger.addHandler(
+    AzureEventHandler(
+        connection_string="InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24"
+    )
+)
 # Set the logging level
 logger.setLevel(logging.INFO)
 
 # Add Metrics
 exporter = metrics_exporter.new_metrics_exporter(
-enable_standard_metrics=True,
-connection_string='InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24')
+    enable_standard_metrics=True,
+    connection_string="InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24",
+)
 view_manager.register_exporter(exporter)
 
 # Add Tracing
 tracer = Tracer(
- exporter=AzureExporter(
-     connection_string='InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24'),
- sampler=ProbabilitySampler(1.0),
+    exporter=AzureExporter(
+        connection_string="InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24"
+    ),
+    sampler=ProbabilitySampler(1.0),
 )
-
 app = Flask(__name__)
 
 # Add Requests
 middleware = FlaskMiddleware(
- app,
- exporter=AzureExporter(connection_string="InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24"),
- sampler=ProbabilitySampler(rate=1.0)
+    app,
+    exporter=AzureExporter(
+        connection_string="InstrumentationKey=e1e0aadc-8061-4207-8d62-368d500a6f24"
+    ),
+    sampler=ProbabilitySampler(rate=1.0),
 )
 
 # Load configurations from environment or config file
-app.config.from_pyfile('config_file.cfg')
+app.config.from_pyfile("config_file.cfg")
 
-if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
-    button1 = os.environ['VOTE1VALUE']
+if "VOTE1VALUE" in os.environ and os.environ["VOTE1VALUE"]:
+    button1 = os.environ["VOTE1VALUE"]
 else:
-    button1 = app.config['VOTE1VALUE']
+    button1 = app.config["VOTE1VALUE"]
 
-if ("VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']):
-    button2 = os.environ['VOTE2VALUE']
+if "VOTE2VALUE" in os.environ and os.environ["VOTE2VALUE"]:
+    button2 = os.environ["VOTE2VALUE"]
 else:
-    button2 = app.config['VOTE2VALUE']
+    button2 = app.config["VOTE2VALUE"]
 
-if ("TITLE" in os.environ and os.environ['TITLE']):
-    title = os.environ['TITLE']
+if "TITLE" in os.environ and os.environ["TITLE"]:
+    title = os.environ["TITLE"]
 else:
-    title = app.config['TITLE']
+    title = app.config["TITLE"]
 
 # Redis configurations
-redis_server = os.environ['REDIS']
+redis_server = os.environ["REDIS"]
 
 # Redis Connection
 try:
     if "REDIS_PWD" in os.environ:
-        r = redis.StrictRedis(host=redis_server,
-                        port=6379,
-                        password=os.environ['REDIS_PWD'])
+        r = redis.StrictRedis(
+            host=redis_server, port=6379, password=os.environ["REDIS_PWD"]
+        )
     else:
         r = redis.Redis(redis_server)
     r.ping()
 except redis.ConnectionError:
-    exit('Failed to connect to Redis, terminating.')
+    exit("Failed to connect to Redis, terminating.")
 
 # Change title to host name to demo NLB
-if app.config['SHOWHOST'] == "true":
+if app.config["SHOWHOST"] == "true":
     title = socket.gethostname()
 
 # Init Redis
-if not r.get(button1): r.set(button1,0)
-if not r.get(button2): r.set(button2,0)
+if not r.get(button1):
+    r.set(button1, 0)
+if not r.get(button2):
+    r.set(button2, 0)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
 
-    if request.method == 'GET':
+    if request.method == "GET":
 
         # Get current values
-        vote1 = r.get(button1).decode('utf-8')
+        vote1 = r.get(button1).decode("utf-8")
         # TODO: use tracer object to trace cat vote
         with tracer.span(name="Cats Vote") as span:
             print("Cats Vote")
 
-        vote2 = r.get(button2).decode('utf-8')
+        vote2 = r.get(button2).decode("utf-8")
         # TODO: use tracer object to trace dog vote
         with tracer.span(name="Dogs Vote") as span:
             print("Dogs Vote")
 
         # Return index with values
-        return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+        return render_template(
+            "index.html",
+            value1=int(vote1),
+            value2=int(vote2),
+            button1=button1,
+            button2=button2,
+            title=title,
+        )
 
-    elif request.method == 'POST':
+    elif request.method == "POST":
 
-        if request.form['vote'] == 'reset':
-            
+        if request.form["vote"] == "reset":
+
             # Empty table and return results
-            r.set(button1,0)
-            r.set(button2,0)
-            
-            vote1 = r.get(button1).decode('utf-8')
-            properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            r.set(button1, 0)
+            r.set(button2, 0)
+
+            vote1 = r.get(button1).decode("utf-8")
+            properties = {"custom_dimensions": {"Cats Vote": vote1}}
             # TODO: use logger object to log cat vote
-            logger.info('Cats Vote', extra=properties)
+            logger.info("Cats Vote", extra=properties)
 
-            vote2 = r.get(button2).decode('utf-8')
-            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            vote2 = r.get(button2).decode("utf-8")
+            properties = {"custom_dimensions": {"Dogs Vote": vote2}}
             # TODO: use logger object to log dog vote
-            logger.info('Dogs Vote', extra=properties)
+            logger.info("Dogs Vote", extra=properties)
 
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-        
+            return render_template(
+                "index.html",
+                value1=int(vote1),
+                value2=int(vote2),
+                button1=button1,
+                button2=button2,
+                title=title,
+            )
+
         else:
 
             # Insert vote result into DB
-            vote = request.form['vote']
-            r.incr(vote,1)
-            
+            vote = request.form["vote"]
+            r.incr(vote, 1)
+
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
@@ -159,10 +186,17 @@ def index():
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
-            logger.info('Dogs Vote', extra=properties)  
+            logger.info('Dogs Vote', extra=properties)    
 
             # Return results
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+            return render_template(
+                "index.html",
+                value1=int(vote1),
+                value2=int(vote2),
+                button1=button1,
+                button2=button2,
+                title=title,
+            )
 
 if __name__ == "__main__":
     app.run()
